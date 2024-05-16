80bda49e641819959cb4f41f179f6a15c898ae2f
bingual <bingual76@gmail.com>
2024-05-17 02:00:04 +0900

    yarn -> pnpm, not page props

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,4 @@
-import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from 'next';
 import type { NextAuthOptions } from 'next-auth';
-import { getServerSession } from 'next-auth';
 import KakaoProvider from 'next-auth/providers/kakao';
 import NaverProvider from 'next-auth/providers/naver';
 
@@ -16,13 +14,3 @@ export const authOptions = {
     }),
   ],
 } satisfies NextAuthOptions;
-
-// Use it in server contexts
-export function getServerAuthSession(
-  ...args:
-    | [GetServerSidePropsContext['req'], GetServerSidePropsContext['res']]
-    | [NextApiRequest, NextApiResponse]
-    | []
-) {
-  return getServerSession(...args, authOptions);
-}
